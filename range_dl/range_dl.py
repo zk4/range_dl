179df243165fe8c4ca6aed3185974e2647fda5d1
zk <liuzq7@gmail.com>
2020-04-17 22:46:03 +0800

    add demo

--- range_dl/range_dl.py
+++ range_dl/range_dl.py
@@ -250,7 +250,7 @@ def parseCurl(curl_str):
         else:
             yield "url",url
     else:
-        raise Exception("no url")
+        raise Exception("no url in curl")
 
 
 
@@ -299,11 +299,14 @@ def entry_point():
 
 
 def createParse():
-    parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="")
+    parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="""
+    range_dl -c "curl  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3628.2 Safari/537.36' -H 'Referer: https://www.bilibili.com/video/BV1RE411k7aL?p=9'  'http://upos-sz-mirrorhw.bilivideo.com/upgcxcode/76/51/127435176/127435176-1-30066.m4s?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M=&uipk=5&nbs=1&deadline=1587141766&gen=playurl&os=hwbv&oi=1875304758&trid=3f395db327314ad499b5d607fe81aca9u&platform=pc&upsig=135d6d1c9664c0b496a7bd5a80051d93&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&mid=0&logo=80000000'" | mpv -
+
+            """)
     parser.add_argument("url",  help="url")
     parser.add_argument('-o', '--out_path',type=str,  help="output path, ex: ./a.mp4" )
     parser.add_argument('-p', '--proxy',type=str,  help="for example: socks5h://127.0.0.1:5992")
-    parser.add_argument('-c', '--curl', help='curl', action='store_true')  
+    parser.add_argument('-c', '--curl', help='enalbe curl command in url! parse only header and url', action='store_true')  
     parser.add_argument('-H', '--headers',type=str,  help="""headers width dict string '{"Host": "qdall01.baidupcs.com"}'""" )
     parser.add_argument('-t', '--threadcount',type=int,  help="thread count" ,default=10)
     parser.add_argument('-d', '--debug', help='debug info', default=False, action='store_true') 
